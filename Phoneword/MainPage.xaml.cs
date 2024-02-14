b67eb691882e5c8b915afa8ef254ccaef5f3028d
Alistair Tait <Alistair.Tait@bglgroup.co.uk>
2024-02-14 17:29:42 +0000

    Initial commit

---
+++ Phoneword/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace Phoneword;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    string translatedNumber;
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = $"Call {translatedNumber}";
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+
+    async void OnCall(object sender, EventArgs e)
+    {
+        if (await this.DisplayAlert(
+            "Dial a Number",
+            $"Would you like to call {translatedNumber}?",
+            "Yes",
+            "No"))
+        {
+            // TODO: dial the phone
+        }
+    }
+}
